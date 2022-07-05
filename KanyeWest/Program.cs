bec84517a1b1376285b07ef3cd71006b41febb27
Katelind Phillips <scarlettkate94@gmail.com>
2022-07-05 14:30:07 -0400

    finalized

---
+++ KanyeWest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var client = new HttpClient();
+            var newQuote = new QuoteGen(client);
+
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.WriteLine($"Kanye West: {newQuote.Kanye()}");
+                Console.WriteLine($"Ron Swanson: {newQuote.RonSwanson()}");
+            }
+
+        }
+    }
+}
+
---
+++ KanyeWest/QuoteGen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWest
+{
+    public class QuoteGen
+    {
+        private HttpClient _client;
+
+        public QuoteGen(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public string Kanye()
+        {
+            //Creating Kanye West
+            var kanyeURL = "https://api.kanye.rest/";
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public string RonSwanson()
+        {
+            //Creating Ron Swanson
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse);
+
+            return ronQuote[0].ToString();
+        }
+    }
+}
+
